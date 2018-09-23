4ce509ee146f29317c62821d35964723bbf4e961
WhalePower <edmundpunch5@gmail.com>
2018-09-22 22:19:27 -0400

    2D-UFO

---
+++ 2D UFO/Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Called after all Update functions have been called every frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ 2D UFO/Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+
+	private int count;
+	public Text countText;
+	public Text winText;
+	public float speed;
+
+
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString();
+		if(count >= 9) {
+			winText.text = "You win!";
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+		count = 0;
+		winText.text = "";
+		SetCountText();
+	}
+	
+	// Happens before any physics calculation
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+	rb2d.AddForce(movement * speed/2);
+	}
+
+	void OnTriggerEnter2D(Collider2D hi){
+		if(hi.gameObject.CompareTag("Pickup")){
+			hi.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+	}
+}
